ab09e357a56162a83a00f41e5e85791ecfce1062
Josh Medeiros <josh@jamedeus.com>
2023-09-22 20:37:29 -0700

    Add tailwind config, remove CDN

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'templates/index.html',
+    'static/history.js',
+    'static/record.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
